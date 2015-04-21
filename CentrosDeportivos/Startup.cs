e287980488ca5915fee215eb6e220763e7436026
Norman <ua.norman@gmail.com>
2015-04-21 13:25:43 +0200

    Inicio del proyecto (plantilla por defecto)

---
+++ CentrosDeportivos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CentrosDeportivos.Startup))]
+namespace CentrosDeportivos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
