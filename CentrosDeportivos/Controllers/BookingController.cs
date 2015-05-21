f13edd387aa5ba798b9e9c82cf8f8bfdf5284a3d
Norman <ua.norman@gmail.com>
2015-05-21 16:33:33 +0200

    Añadido controlador booking para cargar la vista

---
+++ CentrosDeportivos/Controllers/BookingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CentrosDeportivos.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
