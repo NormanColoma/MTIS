9dabc911719a913fa8cd9a94923094ea56ce46b9
Jaroboss <jaromuse@gmail.com>
2015-05-22 18:20:28 +0200

    Register Form implementado con helper

--- CentrosDeportivos/Models/AccountViewModels.cs
+++ CentrosDeportivos/Models/AccountViewModels.cs
@@ -64,10 +64,15 @@ namespace CentrosDeportivos.Models
         [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirmar contraseña")]
-        [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
-        public string ConfirmPassword { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-ZÁÉÍÓÚáéíóuñÑ''-'\s_ ]*$", ErrorMessage = "Introduce un nombre válido")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [RegularExpression("^(([A-Z]\\d{8})|(\\d{8}[A-Z]))$", ErrorMessage = "DNI incorrecto")]
+        [Display(Name = "Ingrese su DNI")]
+        public string DNI { get; set; }
     }
 
     public class ResetPasswordViewModel
