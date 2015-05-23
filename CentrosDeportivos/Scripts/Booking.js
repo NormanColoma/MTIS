9e5a55b31797ccfa0a09df2e700e60f99d986f87
Norman <ua.norman@gmail.com>
2015-05-23 17:30:05 +0200

    Implementado servicio REST para la obtención de los deportes de un centro

---
+++ CentrosDeportivos/Scripts/Booking.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#centers").change(function () {
+        var center = $("#centers option:selected").val();
+        getSports(center);
+    })
+})
+
+function getSports(center) {
+    var port = location.port; //Obtenemos el puerto
+    var uri = "http://localhost:" + port + "/api/SportCenter/"+center; //Dirección del servicio REST
+    $.getJSON(uri) //Hacemos petición GET al servicio
+        .done(function (data) {
+            var sports = data;  //Obtenemos los datos
+            addSports(sports)   //Llamamos a la función para que haga lo que proceda
+                                      
+        })
+        .fail(function (jqXHR, textStatus, err) {  //Si hay error mostramos un mensaje
+                               
+        });
+}
+
+function addSports(sports) {
+    for (var i = 0; i < sports.length; i++) {
+        var sport = '<option value="' + sports[i].idField + '">' + sports[i].nameField + '</option>';  //Creamos el li
+        $("#sports").append(sport); //Añadimos el li a la lista (ul)
+    }
+}
