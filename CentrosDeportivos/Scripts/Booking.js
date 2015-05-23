ba76aedac497ef7518c454964732e95b83c3ec86
Norman <ua.norman@gmail.com>
2015-05-23 17:41:12 +0200

    Preparado el script para completarlo con la disponibilidad de campos

--- CentrosDeportivos/Scripts/Booking.js
+++ CentrosDeportivos/Scripts/Booking.js
@@ -3,6 +3,10 @@
         var center = $("#centers option:selected").val();
         getSports(center);
     })
+
+    $(".available-fields").click(function () {
+        //TODO Aquí deberías obtner el día y la hora, para luego pasárselo a la función que compruebe la disponibilidad;
+    })
 })
 
 function getSports(center) {
@@ -20,8 +24,23 @@ function getSports(center) {
 }
 
 function addSports(sports) {
+    emptySportList();
     for (var i = 0; i < sports.length; i++) {
         var sport = '<option value="' + sports[i].idField + '">' + sports[i].nameField + '</option>';  //Creamos el li
         $("#sports").append(sport); //Añadimos el li a la lista (ul)
     }
+}
+
+function emptySportList() {  //Vaciamos la lista, y añadimos la opción por defecto
+    $("#sports").empty();
+    var default_option = '<option>Selecciona el deporte</option>';
+    $("#sports").append(default_option);
+}
+
+function checkAvailability(date) {
+    //TODO-Aquí es donde tienes que hacer la petición GET al servicio REST como hago yo en el método getSports(center)
+}
+
+function addFields(fields) {
+    //TODO-Aquí tienes que añadir al select de campos disponibles los campos disponibles tras la consumición del servicio
 }
