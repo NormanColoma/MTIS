e1b7742bc6e4a6853a3c8649d422963cc85ecc5f
Norman <ua.norman@gmail.com>
2015-05-25 23:58:51 +0200

    Añadida referencia de servico y script para consumir servico REST

---
+++ CentrosDeportivos/Scripts/matchmaking-searcher.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(".search-teams").click(function () {
+        var sport = $(".sports option:selected").text();
+        getTeams(sport);
+    })
+})
+
+function getTeams(sport) {
+    var port = location.port; //Obtenemos el puerto
+    var uri = "http://localhost:" + port + "/api/Matchmaking/getTeams/" + sport; //Dirección del servicio REST
+    $.getJSON(uri) //Hacemos petición GET al servicio
+        .done(function (data) {
+            alert(data.length)
+            //var teams = data;  //Obtenemos los datos
+            //addSports(sports)   //Llamamos a la función para que haga lo que proceda
+
+        })
+        .fail(function (jqXHR, textStatus, err) {  //Si hay error mostramos un mensaje
+
+        });
+}
